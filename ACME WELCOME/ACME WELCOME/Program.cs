96b541ff8da1b0aa08f799d193743d92031ce36a
John R Adams <johnrusselladamsjr@gmail.com>
2019-06-01 15:10:39 +0300

    Upload ACME WELCOME Assignment to GitHub

---
+++ ACME WELCOME/ACME WELCOME/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Welcome to ACME Accounting Systems");
+        Console.WriteLine(" ");
+        Console.WriteLine("Remember, We're accounting on you!");
+        Console.ReadLine();
+    }
+}
