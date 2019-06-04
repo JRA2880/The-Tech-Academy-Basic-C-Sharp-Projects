33abcd5680bddf66d14755a558a776141b7f8a27
John R Adams <johnrusselladamsjr@gmail.com>
2019-06-04 22:39:05 +0300

    Updated Program 7 file

--- Program7StringListExercise2/Program7StringListExercise2/Program.cs
+++ Program7StringListExercise2/Program7StringListExercise2/Program.cs
@@ -8,6 +8,9 @@
 //1.) Create List of strings that has a at least two identical strings in the List
 //2.) Ask the user to select text to search for in the List. 
 //3.) Create a loop that iterates through the list and then displays the indices of the array that contain matching text on the screen.
+//4.) Add code to that above loop that tells a user if they put in text that isn’t in the List.
+
+
 
 using System;
 using System.Collections.Generic;
@@ -27,14 +30,24 @@ namespace Program7StringListExercise2
 
             Console.WriteLine("Please enter a list item to display the grocery list item and their indexes >>");
             listItem = Console.ReadLine();
-
-                foreach (string listitem in groceryList )
-                {  
-                    index = groceryList.FindIndex(a => a.Contains(listitem));
-                    Console.WriteLine(listitem + " " + index);
-                    
-                
+            if (groceryList.Contains(listItem))
+            {
+                foreach (string item in groceryList)
+                {
+                    index = groceryList.FindIndex(a => a.Contains(item));
+                    if (index >= 0)
+                    {
+                        Console.WriteLine(item + " " + index);
+                    }
                 }
+            }
+            else
+            {
+                Console.WriteLine("Sorry {0} is not on the list.", listItem);
+            }
+
+                
+            
         }
     }
 }
