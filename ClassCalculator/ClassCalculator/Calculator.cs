eb4529e2a79adb820223a69287430206cff31b85
John R Adams <johnrusselladamsjr@gmail.com>
2019-06-06 00:38:38 +0300

    Update Class Calculator to GitHub

---
+++ ClassCalculator/ClassCalculator/Calculator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassCalculator
+{
+    class Calculator
+    {
+
+        public int Add (int a)
+        {
+            int b = a + a;
+            return b;
+        }
+        public int Subtract (int a)
+        {
+            int b = a - a;
+            return b;
+        }
+        public int Multiply(int a)
+        {
+            int b = a * a;
+            return b;
+        }
+    }
+}
---
+++ ClassCalculator/ClassCalculator/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            int number;
+            Calculator calculator = new Calculator();
+
+            Console.WriteLine("Please enter a number for the class calculator to perform some math functions >> ");
+            input = Console.ReadLine();
+            number = Convert.ToInt32(input);
+            int a = calculator.Add(number);
+            int b = calculator.Subtract(number);
+            int c = calculator.Multiply(number);
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+
+            //Console.WriteLine("The result of adding " + number + " to itself is : " + calculator.);
+            Console.ReadLine();
+        }
+    }
+}
