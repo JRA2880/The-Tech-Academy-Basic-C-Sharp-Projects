687c1c3ef203aab01e433c91f32e5ab8d3257851
John R Adams <johnrusselladamsjr@gmail.com>
2019-06-06 19:55:19 +0300

    Upload EmployeePersonClass Assignment to GitHub

---
+++ EmployeePersonClass/EmployeePersonClass/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeePersonClass
+{
+    class Employee : Person
+    {
+        public int Id { get; set; }
+        
+        public void EmployeeId()
+        {
+          Console.WriteLine("Employe ID #: [ {0} ]",Id);
+        }
+    }
+}
