d427d30df72c458c7424e35b1cfae88846fa3505
John R Adams <johnrusselladamsjr@gmail.com>
2019-06-06 23:00:20 +0300

    Update EmployeeQuittableInterface to GitHub

---
+++ EmployeeIQuittableInterface/EmployeeIQuittableInterface/Employee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeIQuittableInterface
+{
+    public class Employee : Person, IQuittable
+    {
+        public string quitReason { get; set; }
+        public string notice { get; set; }
+        public string resignation { get; set; }
+        public override void SayName()
+        {
+            Console.WriteLine("First Name: {0}", firstName);
+            Console.WriteLine("Last Name:  {0}", lastName);
+        }
+
+            public void Quit(Employee employee)
+            {
+                Console.WriteLine("Employe {0} {1} decides to: \n ", firstName, lastName);
+                Console.WriteLine("Quit...");
+                Console.WriteLine("Employee reason for quitting: {0}", quitReason);
+                Console.WriteLine("Employee length of notice: {0}", notice);
+                Console.WriteLine("Employee resignation: {0}", resignation);
+            }
+    }
+          
+}
+
---
+++ EmployeeIQuittableInterface/EmployeeIQuittableInterface/Program.cs
@@ -0,0 +1,47 @@
+﻿//Programmer: John R. Adams
+//Date: 06-06-2019
+//
+//EXERCISE:
+//
+//IN A CONSOLE APP, CREATE CODE THAT DOES THE FOLLOWING:
+//
+//1.)  Create an interface called IQuittable and have it define a void method called Quit().
+//
+//2.) Have your Employee class from the previous drill inherit that interface and implement the Quit() method in any way you choose.
+//
+//3.) Use polymorphism to create an object of type IQuittable and call the Quit() method on it. Hint: an object can be of an interface type if it implements that specific interface.
+
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeIQuittableInterface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            employee.firstName = "John";
+            employee.lastName = "Adams";
+            employee.quitReason = "I have decided to pursue my dream as a software developer, so I am attending the Tech Academy!";
+            employee.notice = "6 month notice of resignation";
+            employee.resignation = "It has been a pleasure to work here, but I have decided to move on from teaching in the Arabian Gulf.  Thank you for the opportunity.";
+            employee.SayName();
+            Console.WriteLine();
+
+            IQuittable IQuit = employee;
+
+            IQuit.Quit(employee);
+
+            
+
+            
+
+        }
+    }
+}
