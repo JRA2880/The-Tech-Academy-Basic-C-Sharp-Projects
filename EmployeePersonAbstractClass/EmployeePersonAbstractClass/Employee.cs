59af3a88f97f813a7be225f3224036e0478b5d0a
John R Adams <johnrusselladamsjr@gmail.com>
2019-06-06 21:41:15 +0300

    Upload EmployeePersonAbstractClass File to GitHub

---
+++ EmployeePersonAbstractClass/EmployeePersonAbstractClass/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeePersonAbstractClass
+{
+    class Employee : Person
+    {
+        public override void SayName()
+        {
+            Console.WriteLine("First Name: {0} ",firstName);
+            Console.WriteLine("Last Name:  {0} ", lastName);
+        }
+    }
+}
