61944b6edc84582c1723ad276f191223229c769d
John R Adams <johnrusselladamsjr@gmail.com>
2019-06-10 17:18:04 +0300

    Update TwentyOne to GitHub

--- TwentyOne/TwentyOne/Program.cs
+++ TwentyOne/TwentyOne/Program.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿using Casino.TwentyOne;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TwentyOne
+namespace Casino
 {
     class Program
     {
