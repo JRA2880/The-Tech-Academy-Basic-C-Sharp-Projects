1de173a8c543d5bc9989d3282b2bc8c22663607e
John R Adams <johnrusselladamsjr@gmail.com>
2019-06-11 18:49:59 +0300

    Update TwentyOne File to GitHub

--- TwentyOne/TwentyOne/Program.cs
+++ TwentyOne/TwentyOne/Program.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using Casino;
 
 namespace TwentyOne
 {
@@ -11,11 +13,8 @@ namespace TwentyOne
     {
         static void Main(string[] args)
         {
-            const string casinoName = "Grand Hotel and Casino";
-
             Console.WriteLine("Welcome to the Grand Hotel and Cansino. Let's start by telling me your name >> ");
             string playerName = Console.ReadLine();
-
             bool validAnswer = false;
             int bank = 0;
             while (!validAnswer)
@@ -27,9 +26,6 @@ namespace TwentyOne
                     Console.WriteLine("Please digits only, decimals >> ");
                 }
             }
-
-            Console.WriteLine("And how much money did you bring today? ");
-            int bank = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Hello, {0}. Would you like to join a game of 21 right now? >> ", playerName);
             string answer = Console.ReadLine().ToLower();
             if (answer == "yes" || answer == "yeah" || answer == "y" || answer == "ya" || answer == "yup")
